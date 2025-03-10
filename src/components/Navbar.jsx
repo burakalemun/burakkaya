c4fa2a478bd863523e242aa154a1658dfa23aade
burakalemun <61062491+burakalemun@users.noreply.github.com>
2025-03-10 22:18:50 +0300

    main

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import logo from "/src/assets/BK.png";
 import { FaLinkedin, FaGithub, FaTwitterSquare, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
+                <img className="mx-2 w-10" src={logo} alt="Logo" />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
                 <a href="https://www.linkedin.com/in/burak-kaya-785b14199/" target="_blank" rel="noopener noreferrer">
