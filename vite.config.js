8c00604b717ffc9f5f9ea3396f33222864cfd2f7
burakalemun <61062491+burakalemun@users.noreply.github.com>
2025-03-10 22:10:43 +0300

    main

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/burakkaya.github.io/', // burası senin repo isminle birebir aynı olacak
+  base: '/burakkaya.github.io/',
   plugins: [
     tailwindcss(),
     react()
   ],
 })
-
